2d0147b304e4cbb775f7564197cc4c927492d728
Elena Berezina <farfalina1908@gmail.com>
2023-06-16 13:59:11 +0300

    update dependencies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
+import React from 'react';
+import ContactsList from './Phonebook/ContactsList/ContactsList';
+import Form from './Phonebook/Form/Form';
+import css from './Phonebook.module.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.container}>
+      <Form />
+      <ContactsList title="Contacts" />
     </div>
   );
 };
---
+++ src/redux/store.jsx
@@ -0,0 +1,27 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import { contactsReducer, filterSlice } from './slicer';
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterSlice.reducer,
+  },
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
+});
+
+export const persistor = persistStore(store);
